ebf38b33323724d33a812b937d8491f1cc8ddfac
Rami Elwan <ramielwan48@gmail.com>
2025-05-14 17:24:30 +0200

    feat: simplify reproduction

--- app/repro.tsx
+++ app/repro.tsx
@@ -1,6 +1,6 @@
-import { LegendList, LegendListRenderItemProps } from "@legendapp/list";
 import { useState } from "react";
-import { StyleSheet, Text, View } from "react-native";
+import { Button, StyleSheet, Text, View } from "react-native";
+import { LegendList, LegendListRenderItemProps } from "@legendapp/list";
 
 const now = new Date();
 const INITIAL_ITEMS_COUNTS = 13;
@@ -16,47 +16,29 @@ const INITIAL_DATA = Array.from({ length: INITIAL_ITEMS_COUNTS }).map(
 export default function Repro() {
   const [data, setData] = useState(INITIAL_DATA);
 
-  const onEndReached = () => {
-    const lastDate = data[data.length - 1];
-    const nextData = Array.from({ length: 10 })
-      .fill(0)
-      .map((_, i) => {
-        return new Date(lastDate.getFullYear(), lastDate.getMonth() + i + 1);
-      });
-
-    setTimeout(() => {
-      setData((prevData) => [...prevData, ...nextData]);
-    }, 2000);
-  };
-
-  const onStartReached = () => {
-    const firstDate = data[0];
-    const prevData = Array.from({ length: 10 })
-      .fill(0)
-      .map((_, i) => {
-        return new Date(firstDate.getFullYear(), firstDate.getMonth() - i - 1);
-      });
-    setTimeout(() => {
-      setData([...prevData.reverse(), ...data]);
-    }, 2000);
-  };
-
   return (
     <View style={styles.screen}>
+      <Button
+        title="add items to front"
+        onPress={() => {
+          const firstDate = data[0];
+          const prevData = Array.from({ length: 10 })
+            .fill(0)
+            .map((_, i) => {
+              return new Date(
+                firstDate.getFullYear(),
+                firstDate.getMonth() - i - 1,
+              );
+            });
+          setData([...prevData.reverse(), ...data]);
+        }}
+      />
       <LegendList
         style={styles.list}
         data={data}
-        initialScrollIndex={data.findIndex((item) => {
-          return (
-            item.getFullYear() === now.getFullYear() &&
-            item.getMonth() === now.getMonth()
-          );
-        })}
         maintainVisibleContentPosition
         keyExtractor={(item) => item.toString()}
         renderItem={renderItem}
-        onStartReached={onStartReached}
-        onEndReached={onEndReached}
       />
     </View>
   );
